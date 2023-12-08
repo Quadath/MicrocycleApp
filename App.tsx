0cc785d5bf104059652153af1711b0066734bd6e
Roman <quadath@gmail.com>
2023-12-08 12:47:37 +0300

    start

--- App.tsx
+++ App.tsx
@@ -1,118 +1,70 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, {useState, useEffect} from 'react'
+import {Text, View, Pressable, TextInput, ActivityIndicator, FlatList, SafeAreaView} from 'react-native'
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import ExerciseItem from './Components/ExerciseItem'
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import styles from './Styles/App'
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+type Exercise = {
+    name: string,
+    _id: string
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const App = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState<Exercise[]>([]);
+  const [text, setText] = useState('');
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const getExercises = async () => {
+    try {
+      const response = await fetch('http://95.31.196.92:3000/exercises');
+      const json = await response.json();
+      setData(json);
+      console.log(json);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
   };
 
+  useEffect(() => {
+    getExercises();
+  }, []);
+
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+  <SafeAreaView>
+    <View>
+    <Text style={styles.header_text}>Exercises</Text>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <View>
+        <FlatList
+          data={data}
+          keyExtractor={({_id}) => _id}
+          renderItem={({item}) => (
+          <ExerciseItem name={item.name}/>
+          )}
+        />
+            <View style={styles.new_exercise_block}>
+             <TextInput
+                    style={styles.input}
+                    onChangeText={text => setText(text)}
+                    value={text}
+                    placeholder="New exercise"
+                  />
+              <Pressable>
+                <Text>+</Text>
+              </Pressable>
+             </View>
         </View>
-      </ScrollView>
-    </SafeAreaView>
+      )}
+    </View>
+  </SafeAreaView>
+
   );
-}
+};
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
-export default App;
+module.exports = App;
