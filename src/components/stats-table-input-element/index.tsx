25f1f0d5be720022c6f08840a00d22d81faa03b3
Roman <quadath@gmail.com>
2024-02-16 20:25:41 +0300

    StatsTableInputElement

---
+++ src/components/stats-table-input-element/index.tsx
@@ -0,0 +1,18 @@
+export default function StatsTableInputElement({style, text, date, type, setData, data, editingMode}) {
+
+    function handleChange(e) {
+        setData({
+            ...data,
+            [date]: {
+                ...data[date],
+                [type]: e.target.value
+            }
+        })
+    }
+
+    return (
+        <div className={`exercise-stats-table-item ${editingMode ? "editing" : ""}`} style={style}>
+            {editingMode && type !== "date"? <input onChange={(e) => handleChange(e)} value={text}/> : <span>{text}</span>}
+        </div>
+    )
+}
