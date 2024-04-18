4b57a2e8d72b4f351f60f3c39dea4060d3357e2c
Roman <quadath@gmail.com>
2024-04-18 09:02:13 +0300

    genius system

--- src/components/training-card/TrainingCard.tsx
+++ src/components/training-card/TrainingCard.tsx
@@ -23,17 +23,18 @@ export default function TrainingCard({training, exercises, active, indexDelta}) 
                 <button className="training-card-edit" onClick={() => setEditingMode(!editingMode)}>EDIT</button>
             </div>
             <div className="training-card-content">
-                {!editingMode && days.map(day => <TrainingCardDayItem  exercises={exercises} training={training} 
-                editingMode={editingMode} day={day}/>)}
+                {!editingMode && days.map(day => <TrainingCardDayItem exercises={exercises} training={training} 
+                editingMode={editingMode} day={day} editedData={editedData} setEditedData={setEditedData}/>)}
                 {editingMode && weekDays.map(day => <TrainingCardDayItem  exercises={exercises} training={training} 
-                editingMode={editingMode} day={day}/>)}
+                editingMode={editingMode} day={day} editedData={editedData} setEditedData={setEditedData}/>)}
             </div>
         </div>
     )
 }
 
-function TrainingCardDayItem({exercises, training, day, editingMode}) {
+function TrainingCardDayItem({exercises, training, day, editingMode, editedData, setEditedData}) {
     const [isInputActive, setInputActive] = useState(false);
+    const [inputValue, setInputValue] = useState('')
 
     return (
         <div className="training-card-day">
@@ -42,8 +43,27 @@ function TrainingCardDayItem({exercises, training, day, editingMode}) {
                 {editingMode && <button onClick={() => setInputActive(!isInputActive)} className='training-card-add-exercise-button'>+</button>}
             </h5>
             {training.days[day]?.map((exercise, index) => {
-                return <p key={`${exercise.exerciseID}${day}`}>{index + 1} {exercises[exercise.exerciseID].name }</p>
+                return <p key={`${exercise.exerciseID}${day}`}>{index + 1} {exercises[exercise.exerciseID].name }
+                {editingMode && <button className='training-card-remove-exercise-button'
+                onClick={() => setEditedData({...editedData, removed: {...editedData.removed, [day]: ['hi']}})}
+                >x</button>}
+                </p>
             })}
+            {editingMode && isInputActive && <>
+                <input placeholder='Exercise name...' value={inputValue} 
+                onChange={(e) => setInputValue(e.target.value)}/>
+                {inputValue.length >= 2 && <TrainingCardInputDropdown exercises={exercises} value={inputValue}/>}
+                </>
+            }
+        </div>
+    )
+}
+
+function TrainingCardInputDropdown({exercises, value}) {
+    const matchedExercises = Object.keys(exercises).filter(key => exercises[key].name.toLowerCase().includes(value.toLowerCase()))
+    return (
+        <div className="training-card-input-dropdown">
+            {matchedExercises.map(ex => <p>{exercises[ex].name}</p>)}
         </div>
     )
 }
